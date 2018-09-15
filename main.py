08fc7427423afb66f5273dbb63caa2959dc60c36
brian <43047619+brian201546104@users.noreply.github.com>
2018-09-15 16:38:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def entra():
-    return 'huelladigital' 
 @app.route('/hola')
 def hola():
     return 'hola' 
 @app.route('/metio/<nombre>')
 def metio(nombre):
     return 'inserto %s'% nombre 
-
+@app.route('/')
+def entra():
+    return 'huelladigital' 
 if __name__ == '__main__':
   app.run()
