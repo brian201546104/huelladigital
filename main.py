8c842f670914339326698a8df3650ee8a61badd1
brian <43047619+brian201546104@users.noreply.github.com>
2018-09-15 16:21:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def entra(contra):
     return 'huelladigital' 
-@app.route('/hola')
-def hola(contra):
-    return 'hola' 
-@app.route('/saludo/<nombre>')
-def saludo(nombre):
-    return 'Hola ' + nombre + '!!!'
+
 
 if __name__ == '__main__':
   app.run()
