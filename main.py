b9f7d36f77d8531809efcd16fd249d73789fa3ca
brian <43047619+brian201546104@users.noreply.github.com>
2018-09-15 15:50:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'huella digital casi sale'
+def huella(url='https://huelladigital.azurewebsites.net/',offset=0):
+  args={'offset':offset} if offset else{}
+  return args
 
 if __name__ == '__main__':
   app.run()
