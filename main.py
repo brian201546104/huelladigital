aef465fd3f76e0fb95cb321ceb794758e51752fa
brian <43047619+brian201546104@users.noreply.github.com>
2018-09-15 16:16:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
-
+@app.route('/hola',methods=['GET'])
+def hola(contra):
+    return 'huelladigital' 
 @app.route('/saludo/<nombre>',methods=['GET'])
 def saludo(nombre):
     return 'Hola ' + nombre + '!!!'
